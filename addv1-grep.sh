dcdb7b6092232040975a7908be04967bdb78bbde
oditynet <oditynet@gmail.com>
2024-11-21 10:22:42 +0300

    Add files via upload

--- addv1-grep.sh
+++ addv1-grep.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#TODO: prototype_id found
 
-clustername="ADQM" #завести заранее  Lovely Amur
+#bash addv1-grep.sh --edit - modify hosts
+#bash addv1-grep.sh  - add new hosts
+
+
+clustername="adb-t1" #завести заранее  Lovely Amur
 adcmip="10.6.16.120"  # завести заранее 8000 port
 edit=$1
 if [[ -z $adcmip ]]; then
@@ -33,35 +38,37 @@ ansible_port=$t5
 #echo $jsonadd|jq
 tokens=$(curl -s -X POST -H 'Content-type: application/json' -d '{"username": "admin","password": "admin"}' http://$adcmip:8000/api/v1/token/ )            
 token=$(echo $tokens|awk -F"\"" '{print $4}')
-#echo $token
+echo $token
 clusters=`curl -s -X GET -H 'Content-type: application/json' -H 'Authorization: token '$token''  http://$adcmip:8000/api/v1/cluster/ `
-#echo "Cluster name: "$clusters
+echo "Cluster name: "$clusters
 
 #idcluster=`echo $clusters | jq  --arg clustername "$clustername" ' .[] |select(.name==$clustername) | .id' `
-temp_id=`echo $clusters|grep -Po 'name":"\K[^",]*'|awk 'NR==1{s=0}{s=s+1;print s,$1}'|grep $clustername|awk '{print $1}'`
+#[1]temp_id=`echo $clusters|grep -Po 'name":"\K[^",]*'|awk 'NR==1{s=0}{s=s+1;print s,$1}'|grep $clustername|awk '{print $1}'`
+idcluster=`echo $clusters|grep -Po '\[{\K[^$]*' | sed -r 's/\}\,\{/\n/'| awk -F',' '{print $1":"$3}'|tr -d '"'|awk -F ':' '{print $2" "$4}'|grep " $clustername$"|awk '{print $1}'`
 #echo "temp_id="$temp_id
-idcluster=`echo $clusters|grep -Po '"id":\K[^,]*'|awk 'NR==1{s=0}{s=s+1; if (s=='$temp_id')print $1}' `
+#[1]idcluster=`echo $clusters|grep -Po '"id":\K[^,]*'|awk 'NR==1{s=0}{s=s+1; if (s=='$temp_id')print $1}' `
 
 #typeid=`echo $clusters | jq  --arg clustername "$clustername" ' .[] |select(.name==$clustername) | .prototype_id' `
 provids=`curl -s -X GET -H 'Content-type: application/json' -H 'Authorization: token '$token''  http://$adcmip:8000/api/v1/provider/`
 #provid=`echo $provids | jq  ' .[].id' `
 provid=`echo $provids|grep -Po '"id":\K[^,]*' `
+
 echo "[+] Cluster id: "$idcluster
 prototype_ids=`curl -s -X GET -H 'Content-type: application/json' -H 'Authorization: token '$token''  http://$adcmip:8000/api/v1/host/`
 #echo $prototype_ids 
 #prototype_id=`echo $prototype_ids | jq  ' first(.[].prototype_id)' `
 prototype_id=`echo $prototype_ids |grep -Po '"prototype_id":\K[^",]*'|head -1`
-
+#[2]prototype_id=`echo $clusters| sed -r 's/\}\,\{/\n/'| awk -F',' '{print $3":"$2}'|tr -d '"'|grep ":$clustername:"|awk -F ':' '{print $4}'`
 
 echo "[+] Prototype_id: "$prototype_id
 echo "[+] Provider: "$provid
 if [[  -z $edit ]]; then
-    echo "Add new node"
+    echo "[_]   Add new node."
     #add
     #newhost=`curl -s -X POST -H 'Content-type: application/json' -H 'Authorization: token '$token'' -d '{"prototype_id":'$prototype_id', "provider_id": "'$provid'", "cluster_id":"'$idcluster'", "fqdn":"'$hostname'", "header": "init"}' http://$adcmip:8000/api/v1/host/`
     newhost=`curl -s -X POST -H 'Content-type: application/json' -H 'Authorization: token '$token'' -d '{"cluster_id":"'$idcluster'","prototype_id":'$prototype_id',  "provider_id": "'$provid'","fqdn":"'$hostname'", "header": "init"}' http://$adcmip:8000/api/v1/host/`
     
-    #echo $newhost
+    echo $newhost
     #idnewhost=`echo $newhost | jq  ' .id'`
     idnewhost=`echo $newhost |grep -Po '"id":\K[^,]*' `
 
